1dc76ae7c469d4ae49e9d3aa8ef3c79ae30aea00
Athulya Peter <athulyaor@gmail.com>
2021-06-10 07:28:16 +0400

    Initial commit

--- app/src/androidTest/java/com/example/myclassifieds/DetailActivityTest.kt
+++ app/src/androidTest/java/com/example/myclassifieds/DetailActivityTest.kt
@@ -1,6 +1,7 @@
 package com.example.myclassifieds
 
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
@@ -27,6 +28,11 @@ class DetailActivityTest {
         onView(withId(R.id.detailImage))
             .check(matches(isDisplayed()))
 
+    }
+
+    @Test
+    fun goHome() {
+        onView(withId(R.id.home)).perform(ViewActions.click())
     }
 }
 
